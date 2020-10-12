2bd3a5083399bae7a9ac633cfb5baa21f09a8f39
Zakaria Mejdoub <mejdoub-zakaria@gmx.fr>
2020-10-12 15:09:18 +0200

    Modification du src

--- src/FightActions.java
+++
@@ -1,4 +0,0 @@
-public interface FightActions {
-    public void atk();
-    public void def();
-}
--- src/Personnage.java
+++ src/Personnage.java
@@ -1,2 +1,12 @@
 public class Personnage {
+    private int exp;
+    private int lifePoint;
+    private int def;
+    private int dex;
+    private int str;
+    private String name;
+    public Weapon weapon;
+
+
+
 }
